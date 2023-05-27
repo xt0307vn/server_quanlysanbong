149ff792487caab280633ced4bc65bdf48fdab59
xt0307vn <xt0307vn@gmail.com>
2023-05-27 10:35:24 +0700

    push của chung

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "xuantruong0307@",
+    password: "chung",
     database: "quanlydatsan",
 });
 
