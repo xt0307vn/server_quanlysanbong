53b4abf242d242624f5335d7b6e489a5cf7b617a
xt0307vn <xt0307vn@gmail.com>
2023-06-11 16:54:09 +0700

    Push

--- index.js
+++ index.js
@@ -133,8 +133,8 @@ app.get("/getWard", (req, res) => {
     });
 });
 
-app.post("/user", (req, res) => {
-    const q = "INSERT INTO `quanlydatsan`.`taikhoan` (`tentaikhoan`, `matkhau`, `email`, `sdt`, `id_trangthai_taikhoan`, `anhdaidien`, `hoten`, `id_xaphuong`, `id_loaitaikhoan`, `diachicuthe`) VALUES ( ? );"
+app.post("/account", (req, res) => {
+    const q = "INSERT INTO `quanlydatsan`.`taikhoan` (`tentaikhoan`, `matkhau`, `email`, `sdt`, `id_trangthai_taikhoan`, `anhdaidien`, `hoten`, `id_xaphuong`, `id_loaitaikhoan`, `diachicuthe`, `ngaysinh`) VALUES ( ? );"
 
     const value = [
         req.body.tentaikhoan,
@@ -146,7 +146,8 @@ app.post("/user", (req, res) => {
         req.body.hoten,
         req.body.id_xaphuong,
         req.body.id_loaitaikhoan,
-        req.body.diachicuthe
+        req.body.diachicuthe,
+        req.body.ngaysinh
     ]
     db.query(q, [value], (err, data) => {
         if(err) return res.json(err)
@@ -154,5 +155,133 @@ app.post("/user", (req, res) => {
             status: 200,
             content: "Thêm tài khoản thành công"
         })
+    });
+});
+
+app.delete("/account/:idAccount", (req, res) => {
+    const q = "DELETE FROM taikhoan WHERE id_taikhoan = ?"
+    const idAccount = req.params.idAccount
+    db.query(q, [idAccount]), (err, data) => {
+        if (err) return res.json(err)
+        return res.json({
+            status: 200,
+            message: "Xóa tài khoản thành công"
+        })
+    }
+})
+
+
+app.put("/account/:idAccount", (req, res) => {
+    const q = "UPDATE `taikhoan` SET `tentaikhoan` = ?, `matkhau` = ?, `email` = ?, `sdt` = ?, `ngaysinh` = ?, `id_trangthai_taikhoan` = ?, `anhdaidien` = ?, `hoten` = ?, `id_xaphuong` = ?, `id_loaitaikhoan` = ?, `diachicuthe` = ? WHERE (`id_taikhoan` = ?);"
+
+
+    const idAccount = req.params.idAccount
+    const value = [
+        req.body.tentaikhoan,
+        req.body.matkhau,
+        req.body.email,
+        req.body.sdt,
+        req.body.ngaysinh,
+        req.body.id_trangthai_taikhoan,
+        req.body.anhdaidien,
+        req.body.hoten,
+        req.body.id_xaphuong,
+        req.body.id_loaitaikhoan,
+        req.body.diachicuthe,
+        
+    ]
+    db.query(q, [...value, idAccount]), (err, data) => {
+        if (err) return res.json(err)
+        return res.json({
+            status: 200,
+            message: "Cập nhật tài khoản thành công"
+        })
+    }
+})
+
+app.get("/checkExsitsUser", (req, res) => {
+    const query = "SELECT * FROM taikhoan where tentaikhoan = ?";
+    const username = req.query.username;
+    db.query(query, [username], (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data);
+    });
+});
+
+app.get("/accounts", (req, res) => {
+    const query = "SELECT * FROM taikhoan;";
+    db.query(query,  (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data);
+    });
+});
+
+app.get("/account", (req, res) => {
+    const query = "SELECT tentaikhoan,matkhau,email, anhdaidien,hoten,sdt, substring(ngaysinh, 9, 2) as ngay, substring(ngaysinh, 6, 2) as thang, substring(ngaysinh, 1, 4) as nam , concat( 'Ngày ',substring(ngaysinh, 9, 2), ' tháng ',substring(ngaysinh, 6, 2),' năm ', substring(ngaysinh, 1, 4)) as 'ngaysinh' , concat(diachicuthe, ', ' , tenxaphuong , ', ', tenquanhuyen, ', ', tentinhthanh) as diachi, xaphuong.id_xaphuong, quanhuyen.id_quanhuyen, tinhthanh.id_tinhthanh, diachicuthe, id_loaitaikhoan FROM taikhoan, xaphuong, quanhuyen, tinhthanh where taikhoan.id_xaphuong = xaphuong.id_xaphuong and xaphuong.id_quanhuyen = quanhuyen.id_quanhuyen and quanhuyen.id_tinhthanh = tinhthanh.id_tinhthanh and id_taikhoan = ?"
+    const idAccount = req.query.idAccount;
+    db.query(query, [idAccount], (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data);
+    });
+});
+
+// SELECT * FROM san order by id_san desc limit 1
+app.get("/footballpitchlast", (req, res) => {
+    const query = "SELECT * FROM san order by id_san desc limit 1";
+    db.query(query,  (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data);
+    });
+});
+
+app.get("/accounts-pendding", (req, res) => {
+    const query = "SELECT * FROM taikhoan, loaitaikhoan, trangthai_taikhoan WHERE taikhoan.id_loaitaikhoan = loaitaikhoan.id_loaitaikhoan and taikhoan.id_trangthai_taikhoan = trangthai_taikhoan.id_trangthai_taikhoan and trangthai_taikhoan.id_trangthai_taikhoan = 3"
+    db.query(query,  (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data);
+    });
+});
+
+
+
+
+app.post("/booking", (req, res) => {
+    const query = "INSERT INTO `quanlydatsan`.`datsan` (`id_san`, `id_taikhoan`, `ngaydat`, `id_trangthai_datsan`, `id_khunggio`) VALUES (?);";
+    const values = [
+        req.body.id_san,
+        req.body.id_taikhoan,
+        req.body.ngaydat,
+        req.body.id_trangthai_datsan,
+        req.body.id_khunggio,
+    ]
+    db.query(query, [values],  (err, data) => {
+        if (err) return res.json(err);
+        return res.json({
+            status: 200,
+            message: "Đặt sân thành công"
+        });
+    });
+});
+
+
+app.put("/accounts-pendding/:id", (req, res) => {
+    const query = "UPDATE taikhoan SET id_trangthai_taikhoan = 1 WHERE  id_taikhoan = ?";
+    const tt = 1
+    const id = req.query.id
+    db.query(query, [id],  (err, data) => {
+        if (err) return res.json(err);
+        return res.json({
+            status: 200,
+            message: "Cập nhật thành công"
+        });
+    });
+});
+
+app.get("/history/:idAccount", (req, res) => {
+    const query = "SELECT * FROM datsan, san, sanbong, khunggio WHERE datsan.id_san = san.id_san and khunggio.id_khunggio = datsan.id_khunggio and san.id_sanbong = sanbong.id_sanbong and datsan.id_taikhoan = ? and id_trangthai_datsan = 4"
+    const idAccount = req.params.idAccount
+    db.query(query, [idAccount],  (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data);
     });
 });
